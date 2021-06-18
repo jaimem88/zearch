0f105de396ab4794a96c902015ceabab218212f4
Jaime Martinez <jaime.martinez88@gmail.com>
2021-06-18 17:01:43 +1000

    Add organizations.go to store

---
+++ internal/store/organizations.go
@@ -0,0 +1,146 @@
+package store
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/jaimem88/zearch/internal/model"
+)
+
+// Organizations implements the searcher method for the app. It searches by term and value.
+// Handles a special case for _id which can be looked up in the Storage easily from the
+// OrganizationsMap.
+func (s *Storage) Organizations(term, value string) ([]model.OrganizationResult, error) {
+	fmt.Printf("Searching organizations by: %q with value: %q\n", term, value)
+
+	if term == "_id" {
+		return s.searchOrgByID(value)
+	}
+
+	return s.searchOrgByTerm(term, value)
+}
+
+func (s *Storage) searchOrgByID(value string) ([]model.OrganizationResult, error) {
+	var results []model.OrganizationResult
+
+	id, err := strconv.Atoi(value)
+	if err != nil {
+		return nil, err
+	}
+
+	orgID := model.OrgID(id)
+
+	org, ok := s.OrganizationsMap[orgID]
+	if !ok {
+		return nil, ErrNotFound
+	}
+
+	orgResult := model.OrganizationResult{
+		Organization:   org,
+		UserNames:      s.getUsersForOrg(orgID),
+		TicketSubjects: s.getTicketsForOrg(orgID),
+	}
+
+	results = append(results, orgResult)
+
+	return results, nil
+}
+
+// searchOrgByTerm will iterate over each element of the OrganizationsMap and accessing
+// the term directly. Once found, the organization and its ID will be saved in a slice to later fetch
+// the related tickets and users.
+func (s *Storage) searchOrgByTerm(term, value string) ([]model.OrganizationResult, error) {
+	type orgAndID struct {
+		id  model.OrgID
+		org model.Organization
+	}
+	var result []model.OrganizationResult
+	var foundOrgs []*orgAndID
+
+	// search all organizations for a match in a specific field
+	for orgID, org := range s.OrganizationsMap {
+		if findOrgMatch(org, term, value) {
+			foundOrgs = append(foundOrgs, &orgAndID{
+				id:  orgID,
+				org: org,
+			})
+		}
+	}
+
+	for _, orgAndID := range foundOrgs {
+		orgResult := model.OrganizationResult{
+			Organization:   orgAndID.org,
+			UserNames:      s.getUsersForOrg(orgAndID.id),
+			TicketSubjects: s.getTicketsForOrg(orgAndID.id),
+		}
+
+		result = append(result, orgResult)
+	}
+
+	return result, nil
+}
+
+func findOrgMatch(org model.Organization, term, value string) bool {
+	foundMatch := false
+
+	switch v := org[term].(type) {
+	case string:
+		foundMatch = v == value
+	case int:
+		foundMatch = strconv.Itoa(v) == value
+	case float64:
+		// assume there are no decimals
+		foundMatch = strconv.Itoa(int(v)) == value
+	case bool:
+		foundMatch = strconv.FormatBool(v) == value
+	case []interface{}:
+		s := ""
+		for _, elem := range v {
+			// assume they are strings and try to format them
+			// and append them to s
+			s = fmt.Sprintf("%s;%s", s, elem)
+		}
+
+		foundMatch = strings.Contains(s, value)
+	default:
+		fmt.Printf("unhandled type for v: %T\n", v)
+	}
+
+	return foundMatch
+}
+
+func (s *Storage) getUsersForOrg(orgID model.OrgID) []string {
+	usersForOrg := s.OrgsUsers[orgID]
+	// initializing a slice with capacity allows us to use `append` preventing it
+	// from allocating a new slice  when the capacity is reached see https://golang.org/pkg/builtin/#append
+	userNames := make([]string, 0, len(usersForOrg))
+	for _, userID := range usersForOrg {
+		user, ok := s.UsersMap[userID]
+		if !ok {
+			// skip if we can't find the userID for some reason
+			continue
+		}
+
+		userNames = append(userNames, user["name"].(string))
+	}
+
+	return userNames
+}
+
+func (s *Storage) getTicketsForOrg(orgID model.OrgID) []string {
+	ticketsForOrg := s.OrgsTickets[orgID]
+	ticketSubjects := make([]string, 0, len(ticketsForOrg))
+	// if we had generics, maybe this could have been implemented once
+	for _, ticketID := range ticketsForOrg {
+		ticket, ok := s.TicketsMap[ticketID]
+		if !ok {
+			// skip if we can't find the ticketID for some reason
+			continue
+		}
+
+		ticketSubjects = append(ticketSubjects, ticket["subject"].(string))
+	}
+
+	return ticketSubjects
+}
--- internal/store/store_test.go
+++ internal/store/store_test.go
@@ -1,117 +1,13 @@
 package store
 
 import (
-	"sort"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/jaimem88/zearch/internal/model"
 	"github.com/jaimem88/zearch/internal/parser"
 )
-
-func TestStorage_Organizations(t *testing.T) {
-	orgData := readOrgs(t)
-	userData := readUsers(t)
-	ticketData := readTickets(t)
-
-	tests := []struct {
-		name           string
-		userData       model.Users
-		ticketData     model.Tickets
-		orgData        model.Organizations
-		term           string
-		value          string
-		expectedResult []*model.OrganizationResult
-		expectedError  error
-	}{
-		{
-			name:    "search_by_id_with_no_users_no_tickets",
-			orgData: orgData,
-			term:    "_id",
-			value:   "101",
-			expectedResult: []*model.OrganizationResult{
-				{
-					Organization: orgData[0],
-				},
-			},
-		},
-		{
-			name:           "search_by_id_does_not_exist",
-			orgData:        orgData,
-			term:           "_id",
-			value:          "0",
-			expectedResult: nil,
-			expectedError:  ErrNotFound,
-		},
-		{
-			name:       "search_by_id_with_users_and_tickets",
-			orgData:    orgData,
-			userData:   userData,
-			ticketData: ticketData,
-			term:       "_id",
-			value:      "101",
-			expectedResult: []*model.OrganizationResult{
-				{
-					Organization:   orgData[0],
-					UserNames:      []string{"Francis Bailey"},
-					TicketSubjects: []string{"A Problem in Guyana"},
-				},
-			},
-		},
-		{
-			name:    "search_by_shared_tickets_boolean",
-			orgData: orgData,
-			term:    "shared_tickets",
-			value:   "true",
-			expectedResult: []*model.OrganizationResult{
-				{
-					Organization: orgData[1],
-				},
-			},
-		},
-		{
-			name:    "search_by_tag_string_slice",
-			orgData: orgData,
-			term:    "tags",
-			value:   "Farley",
-			expectedResult: []*model.OrganizationResult{
-				{
-					Organization: orgData[0],
-				},
-				{
-					Organization: orgData[1],
-				},
-			},
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			s := New(tt.orgData, tt.userData, tt.ticketData)
-			got, err := s.Organizations(tt.term, tt.value)
-			if tt.expectedError != nil {
-				require.EqualError(t, err, tt.expectedError.Error())
-				return
-			}
-
-			require.NoError(t, err)
-			assert.Len(t, got, len(tt.expectedResult))
-
-			// sort by orgID to ensure comparison below will be deterministic
-			sort.SliceStable(got, func(i int, j int) bool {
-				return got[i].Organization["_id"].(float64) <= got[j].Organization["_id"].(float64)
-			})
-
-			for k, expectedResult := range tt.expectedResult {
-				assert.Equal(t, expectedResult.Organization["_id"], got[k].Organization["_id"])
-				assert.ElementsMatch(t, expectedResult.UserNames, got[k].UserNames)
-				assert.ElementsMatch(t, expectedResult.TicketSubjects, got[k].TicketSubjects)
-			}
-		})
-	}
-}
 
 func readOrgs(t *testing.T) model.Organizations {
 	t.Helper()
